583fd661a1db0c9a3a51846b4dc3be6368147988
Himanshu Rajput <himanshurajputstudy01@gmail.com>
2026-05-18 14:18:38 +0530

    Add files via upload

--- Path_parameters.py
+++ Path_parameters.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from rag!")
+from fastapi import FastAPI
+import json
 
+# need a app object which is object of fastapi:
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+# Supporting function for Data Loding:
+def load_data():
+    with open("Info.json", 'r') as f:
+        data = json.load(f)
+
+    return data
+
+# defining route/path for our endpoint:
+@app.get("/")
+def hello():
+    return {'message':'Hello i am Himanshu'}
+
+@app.get("/about")
+def about():
+    return {'message':"MCA student looking for AI role"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
+
+# getting specific data using path parameters:
+@app.get('/person/{person_id}')
+def view_person(person_id):
+    #load full data:
+    data = load_data()
+
+    # find the required data:
+    if person_id in data:
+        return data[person_id]
+    return {'error':"no person found"}
